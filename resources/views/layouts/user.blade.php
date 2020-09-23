4177372d2632d634e507740bd2a263a1ef61b419
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-09-23 09:30:28 -0400

    cambios

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -215,7 +215,7 @@
                                 <li class="menu-lateral-usuario_ul_li" ><a href="{{ url('home') }}"> <img class="menu-lateral-usuario_ul_li_img" src="{{ asset('user/assets/img/ico-editar.png') }}" alt=""><p>Inicio</p></a></li>
                                 {{--<li class="menu-lateral-usuario_ul_li" ><a href="{{ url('/user/offer') }}"> <img class="menu-lateral-usuario_ul_li_img" src="{{ asset('user/assets/img/ico-editar.png') }}" alt=""><p>Ofertas</p></a></li>--}}
                                 <li class="menu-lateral-usuario_ul_li" ><a href="{{ url('/profile/user') }}"> <img class="menu-lateral-usuario_ul_li_img" src="{{ asset('user/assets/img/ico-editar.png') }}" alt=""><p>Mi perfil</p></a></li>
-                                <li class="menu-lateral-usuario_ul_li" ><a href="{{ url('/profile/show/'.\Aut::user()->email) }}"> <img class="menu-lateral-usuario_ul_li_img" src="{{ asset('user/assets/img/ico-editar.png') }}" alt=""><p>Mi Curriculo</p></a></li>
+                                <li class="menu-lateral-usuario_ul_li" ><a href="{{ url('/profile/show/'.\Auth::user()->email) }}"> <img class="menu-lateral-usuario_ul_li_img" src="{{ asset('user/assets/img/ico-editar.png') }}" alt=""><p>Mi Curriculo</p></a></li>
                                 <li class="menu-lateral-usuario_ul_li" ><a href="{{ url('/my-applies') }}"> <img class="menu-lateral-usuario_ul_li_img" src="{{ asset('user/assets/img/control.png') }}" alt=""><p>Mis postulaciones</p></a></li>
                                 <li class="menu-lateral-usuario_ul_li" ><a href="{{ url('/logout') }}"> <img class="menu-lateral-usuario_ul_li_img" src="{{ asset('user/assets/img/logout.png') }}" alt=""><p>Cerrar sesión</p></a></li>
                             </ul>
