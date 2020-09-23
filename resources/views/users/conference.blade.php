30e1b48e009de6200f165c0dd36cd063a45c7f5f
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-09-22 21:42:44 -0400

    cambios

--- resources/views/users/conference.blade.php
+++ resources/views/users/conference.blade.php
@@ -4,24 +4,24 @@
 
     <div class="col-md-12">
 
-        @if(Appointment::where('room_name', $room_name)->count() > 0)
+        @if(App\Appointment::where('room_name', $room_name)->count() > 0)
 
 
             @php
-                $appointment = Appointment::where("room_name", $room_name)->first();
+                $appointment = App\Appointment::where("room_name", $room_name)->first();
             @endphp
 
             @if(\Auth::user()->id == $appointment->user_id || \Auth::user()->id == $appointment->guest_id)
 
-                @if(Carbon::now()->gte($appointment->date_time) && $appointment->date_time->lt($appointment->date_time->addDay())
-
-                    <div id="#meet"></div>
+                @if(Carbon\Carbon::now()->gte($appointment->date_time) && $appointment->date_time->lt($appointment->date_time->addDay()))
+                   
+                    <div id="meet"></div>
 
                 @else
 
-                    @if($appointment->date_time->lt(\Carbon::now())
+                    @if($appointment->date_time->lt(Carbon\Carbon::now()))
                         <h3>La conferencia comienza el {{ $appointment->date_time }}</h3>
-                    @elseif($appointment->date_time->addDay()->gt(\Carbon::now())
+                    @elseif($appointment->date_time->addDay()->gt(Carbon\Carbon::now()))
                         <h3>La conferencia ha expirado</h3>
                     @endif
 
@@ -41,3 +41,24 @@
 
 @endsection
 
+@push('scripts')
+
+    <script src='https://meet.jit.si/external_api.js'></script>
+
+    <script>
+
+        $(document).ready(function(){
+
+            const domain = 'jitsivideo.sytes.net';
+            const options = {
+                roomName: '{{ $room_name }}',
+                height: 700,
+                parentNode: document.querySelector('#meet')
+            };
+            const api = new JitsiMeetExternalAPI(domain, options);
+
+        })
+
+    </script>
+
+@endpush
