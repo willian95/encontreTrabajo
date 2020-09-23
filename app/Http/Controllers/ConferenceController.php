0e854000a0ee2e2ddcf37dc82fc3f30ffa9b1369
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-09-22 22:48:44 -0400

    cambios

--- app/Http/Controllers/ConferenceController.php
+++ app/Http/Controllers/ConferenceController.php
@@ -123,35 +123,8 @@ class ConferenceController extends Controller
             }else
                 return response()->json(["success" => false, "msg" => "Sala no encontrada"]);
             }
+            
 
-
-                @php
-                    
-                @endphp
-
-                @if(\Auth::user()->id == $appointment->user_id || \Auth::user()->id == $appointment->guest_id)
-
-                    @if(Carbon\Carbon::now()->gte($appointment->date_time) && $appointment->date_time->lt($appointment->date_time->addDay()))
-                    
-                        <div id="meet"></div>
-
-                    @else
-
-                        @if($appointment->date_time->lt(Carbon\Carbon::now()))
-                            <h3>La conferencia comienza el {{ $appointment->date_time }}</h3>
-                        @elseif($appointment->date_time->addDay()->gt(Carbon\Carbon::now()))
-                            <h3>La conferencia ha expirado</h3>
-                        @endif
-
-                    @endif
-
-                @else
-                    <h3 class="text-center">Usted no ha sido invitado a esta conferencia</h3>
-                @endif
-
-            @
-
-            @endif
         }
 
     }
