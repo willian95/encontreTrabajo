1e3e36d1f1b3343ead39e54ceb4cd37898ac99e5
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-10-09 16:35:09 -0400

    cambios

--- resources/views/pdf/profile.blade.php
+++ resources/views/pdf/profile.blade.php
@@ -1,10 +1,13 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     </head>
     <body>
         
-        <img src="{{ $user->image }}" alt="" style="width: 120px;">
+        <p class="text-center">
+            <img src="{{ $user->image }}" alt="" style="width: 120px;">
+        </p>
 
     </body>
 </html>
