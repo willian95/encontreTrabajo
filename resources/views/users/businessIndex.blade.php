de2fbc3040bfba1b4526777cd8300ad8d4e1de2a
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-11-06 15:10:31 -0400

    cambios

--- resources/views/users/businessIndex.blade.php
+++ resources/views/users/businessIndex.blade.php
@@ -39,7 +39,7 @@
                                 @{{ offer.views.length }}
                             </td>
                             <td>
-                                <button class="btn btn-info"><i class="fa fa-edit"></i></button>
+                                <button class="btn btn-success" style="border-radius: .25rem"><i class="fa fa-edit"></i></button>
                                 <button class="btn btn-secondary"><i class="fa fa-trash"></i></button>
                             </td>
                         </tr>
