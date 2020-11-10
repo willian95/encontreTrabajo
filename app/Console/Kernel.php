671a078e751de00d299f23cb882154e096f1f1ef
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-11-09 21:03:29 -0400

    cambios

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('task:everyday')->daily()->at("21:03");
+        $schedule->command('task:everyday')->daily()->at("22:05");
     }
 
     /**
